890829f7403e1fbd3fe2c5fac167b88f30aac9e3
Marcos Cantarutti <marcossandim21@gmail.com>
2025-08-25 11:17:14 -0300

    adicionado rota de delete

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreHabitRequest;
+use App\Http\Requests\UpdateHabitRequest;
 use App\Http\Resources\HabitResource;
 use App\Models\Habit;
+use App\Models\HabitLog;
+use GuzzleHttp\Psr7\Request;
 
 class HabitController extends Controller
 {
@@ -19,6 +22,22 @@ class HabitController extends Controller
     public function show(Habit $habit)
     {
         return HabitResource::make($habit);
+    }
+
+    public function update(UpdateHabitRequest $request, Habit $habit)
+    {
+        $data = $request->validated();
+
+        $habit->update($data);
+
+        return HabitResource::make($habit);
+    }
+
+    public function destroy(Habit $habit)
+    {
+        HabitLog::whereHabitId($habit->id)->delete();
+        $habit->delete();
+        return response()->noContent();
     }
 
     public function store(StoreHabitRequest $request)
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\HabitController;
+use App\Models\Habit;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn(): array => [config('app.name')]);
 Route::prefix('/api')->name('api.')->group(function () {
-    Route::get('/habits', [HabitController::class, 'index'])->name('habits.index');
-    Route::get('/habits/{habit:uuid}', [HabitController::class, 'show'])->name('habits.show');
+    // Route::get('/habits', [HabitController::class, 'index'])->name('habits.index');
+    // Route::get('/habits/{habit:uuid}', [HabitController::class, 'show'])->name('habits.show');
 
-    Route::post('/habits', [HabitController::class, 'store'])->name('habits.store');
-    // Route::put('habits/{habit}', [HabitController::class, 'update'])->name('habits.update');
-    // Route::delete('habits/{habit}', [HabitController::class, 'destroy'])->name('habits.destroy');
+    // Route::post('/habits', [HabitController::class, 'store'])->name('habits.store');
+    // Route::put('habits/{habit:uuid}', [HabitController::class, 'update'])->name('habits.update');
+    // Route::delete('habits/{habit:uuid}', [HabitController::class, 'destroy'])->name('habits.destroy');
+
+    Route::apiResource('habits', HabitController::class)->scoped(['habit' => 'uuid']);
 });
