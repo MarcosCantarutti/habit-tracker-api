2b0b4a6396f8daf9c92e2344278bbe1dc0185349
Marcos Cantarutti <marcossandim21@gmail.com>
2025-09-19 09:43:27 -0300

    add returning habits for one user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
